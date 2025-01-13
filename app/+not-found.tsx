0031e433336efc491556b2b9abaef20241e55204
michaelmuthomi <lit911call@gmail.com>
2025-01-14 01:30:20 +0300

    fix: back functionality not working

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,10 +1,13 @@
-import { Link, router, Stack } from 'expo-router';
-import { View, Image } from 'react-native';
-import { Button } from '~/components/ui/button';
-import { Text } from '~/components/ui/text';
-import { H1, P } from '~/components/ui/typography';
+import { Link, Stack } from "expo-router";
+import { View, Image } from "react-native";
+import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
+import { H1, P } from "~/components/ui/typography";
+import { useNavigation } from "@react-navigation/native";
 
 export default function NotFoundScreen() {
+  const navigation = useNavigation();
+
   return (
     <View className="flex-1 justify-center">
       <Stack.Screen options={{ title: "Oops!" }} />
@@ -20,14 +23,12 @@ export default function NotFoundScreen() {
             We couldn't find the page you were looking for
           </H1>
         </View>
-        <Link href="/">
-          <Button
-            onPress={() => router.back()}
-            className="flex-row items-center gap-2 rounded-full bg-white"
-          >
-            <P className="text-black">&larr; Go Back</P>
-          </Button>
-        </Link>
+        <Button
+          className="flex-row items-center gap-2 rounded-full bg-white"
+          onPress={() => navigation.goBack()}
+        >
+          <P className="text-black">&larr; Go Back</P>
+        </Button>
       </View>
     </View>
   );
